754606c4f7ceae8957c7035142e472f47c9c1205
vkc9448 <11416480+vkc9448@users.noreply.github.com>
2021-03-15 13:20:49 -0400

    Uploaded Bus View
    Waiting for material UI

---
+++ src/pages/BusView.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function BusView() {
+  return (
+    <div class="busview">
+      <header>
+        <p>Bus View</p>
+      </header>
+      <p>Bus 1</p>
+      <p>Bus 2</p>
+      <p>Bus 3</p>
+      <div class="busrow">
+        <p>Bus</p>
+      </div>
+
+      <div class="row">
+        <div class="columna">
+          <p>Team</p>
+        </div>
+        <div class="columnb">
+          <p>Name</p>
+        </div>
+        <div class="columnc">
+          <p> EDIT </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="columna_black">
+          <p>Team</p>
+        </div>
+        <div class="columnb_Grey">
+          <p>Name</p>
+        </div>
+        <div class="columnc_darkblue">
+          <p> EDIT </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="columna">
+          <p>Team</p>
+        </div>
+        <div class="columnb">
+          <p>Name</p>
+        </div>
+        <div class="columnc">
+          <p> EDIT </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="columna_black">
+          <p>Team</p>
+        </div>
+        <div class="columnb_Grey">
+          <p>Name</p>
+        </div>
+        <div class="columnc_darkblue">
+          <p> EDIT </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="columna">
+          <p>Team</p>
+        </div>
+        <div class="columnb">
+          <p>Name</p>
+        </div>
+        <div class="columnc">
+          <p> EDIT </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="columna_black">
+          <p>Team</p>
+        </div>
+        <div class="columnb_Grey">
+          <p>Name</p>
+        </div>
+        <div class="columnc_darkblue">
+          <p> EDIT </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BusView;
