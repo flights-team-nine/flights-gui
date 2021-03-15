de2470cfe13f339113a8c53393f312db01e09ce9
vkc9448 <11416480+vkc9448@users.noreply.github.com>
2021-03-15 12:12:46 -0400

    Added missing page
    Added missing page view. Still playing around with react. working on bus view. I wIll join the meeting for material ui

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import LandingPage from "./LandingPage.js"
-import Login from "./Login.js"
-import BusBook from "./BusBook.js"
+import LandingPage from "./LandingPage.js";
+import Login from "./Login.js";
+import BusBook from "./BusBook.js";
+import BusView from "./BusView.js";
+import MissingPage from "./MissingPage.js";
 
 class App extends Component {
-	/*const history = useHistory();
+  /*const history = useHistory();
 
 	const routeChange = () =>{ 
 		let path = `Login`;
 	    history.push(path);
 	}*/
-	render() {
-		return (
-			<Router>
-				<Switch>
-					<Route exact path="/" component={LandingPage} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/busbook" component={BusBook} />
-				</Switch>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/busbook" component={BusBook} />
+          <Route exact path="/busview" component={BusView} />
+          <Route exact path="/missingpage" component={MissingPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
