5b0e081ee5fc416ced00441cc07441eaea5cccb4
Clare Truell <cet2576@rit.edu>
2021-04-20 13:55:12 -0400

    ;

--- src/pages/Documents.js
+++ src/pages/Documents.js
@@ -3,18 +3,7 @@ import { Link } from "react-router-dom";
 import { Button } from '@material-ui/core';
 import Sidebar from "react-sidebar";
 import MenuIcon from '@material-ui/icons/Menu';
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import LandingPage from "./LandingPage.js";
-import Login from "./Login.js";
-import BusBook from "./BusBook.js";
-import BusView from "./BusView.js";
-import TeamView from "./TeamView.js";
-import MissingPage from "./MissingPage.js";
-import Documents from "./Documents.js";
-import PersonInfo from "./PersonInfo.js";
-
+import { BrowserRouter } from "react-router-dom";
 
 class Documents extends React.Component {
 	constructor(props) {
@@ -31,22 +20,10 @@ class Documents extends React.Component {
 
 	render() {
 		return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/busbook" component={BusBook} />
-          <Route exact path="/busview" component={BusView} />
-          <Route exact path="/teamview" component={TeamView} />
-          <Route exact path="/missingpage" component={MissingPage} />
-          <Route exact path="/documents" component={Documents} />
-          <Route exact path="/personinfo" component={PersonInfo} />
-        </Switch>
-      </Router>
 			<div class="documents">
 				<header>
 					<p><Sidebar
-                    sidebar={<div><h4 style={{padding: '0 100px 0 0'}}>Navigation</h4><span style={{fontWeight: 'normal'}}><Link to="/BusBook">Bus Book</Link><br /><Link to="/TeamView">Team View</Link><br /><Link to="/BusView">Veteran View</Link><br /><Link to="/Documents">Documents</Link></span></div>}
+                    sidebar={<div><h4 style={{padding: '0 100px 0 0'}}>Navigation</h4><span style={{fontWeight: 'normal'}}><a href="BusBook.js">Bus Book</a><br /><a href="TeamView.js">Team View</a><br /><a href="BusView.js">Veteran View</a><br /><a href="Documents.js">Documents</a></span></div>}
                       open={this.state.sidebarOpen}
                       onSetOpen={this.onSetSidebarOpen}
                       styles={{ sidebar: { background: "black", color: "white" } }}
--- src/pages/TeamView.js
+++ src/pages/TeamView.js
@@ -10,17 +10,6 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Sidebar from "react-sidebar";
 import MenuIcon from '@material-ui/icons/Menu';
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import LandingPage from "./LandingPage.js";
-import Login from "./Login.js";
-import BusBook from "./BusBook.js";
-import BusView from "./BusView.js";
-import TeamView from "./TeamView.js";
-import MissingPage from "./MissingPage.js";
-import Documents from "./Documents.js";
-import PersonInfo from "./PersonInfo.js";
-
 const useStyles = makeStyles((theme) => ({
   heading: {
     //fontSize: theme.typography.pxToRem(15),
@@ -47,22 +36,10 @@ class TeamView extends React.Component {
 
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/busbook" component={BusBook} />
-          <Route exact path="/busview" component={BusView} />
-          <Route exact path="/teamview" component={TeamView} />
-          <Route exact path="/missingpage" component={MissingPage} />
-          <Route exact path="/documents" component={Documents} />
-          <Route exact path="/personinfo" component={PersonInfo} />
-        </Switch>
-      </Router>
       <div class="teamview">
         <header>
           <p><Sidebar
-                    sidebar={<div><h4 style={{padding: '0 100px 0 0'}}>Navigation</h4><span style={{fontWeight: 'normal'}}><Link to="/BusBook">Bus Book</Link><br /><Link to="/TeamView">Team View</Link><br /><Link to="/BusView">Veteran View</Link><br /><Link to="/Documents">Documents</Link></span></div>}
+                    sidebar={<div><h4 style={{padding: '0 100px 0 0'}}>Navigation</h4><span style={{fontWeight: 'normal'}}><a href="BusBook.js">Bus Book</a><br /><a href="TeamView.js">Team View</a><br /><a href="BusView.js">Veteran View</a><br /><a href="Documents.js">Documents</a></span></div>}
                       open={this.state.sidebarOpen}
                       onSetOpen={this.onSetSidebarOpen}
                       styles={{ sidebar: { background: "black", color: "white" } }}
